6f56b74c9213df54e62e9eeacca70610ad79e16b
Sam.titmuss <ssumtitmas@googlemail.com>
2023-07-12 11:50:25 +0100

    Comiiting changes

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
-
+provider "azurerm" {
+    features{} 
+}
+resource "azurerm_resource_group" "tf" {
+  name = "tf-rg"
+  location = "UK South"
+}
+resource "azurerm_resource_group" "tf1" {
+  name = "tf1-rg"
+  location = azurerm_resource_group.tf.location
+}
