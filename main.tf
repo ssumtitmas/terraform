374ff65158198d66bfa1cf3222e86060b9dd5d71
Sam.titmuss <ssumtitmas@googlemail.com>
2023-07-12 12:05:39 +0100

    Test

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
-    features{} 
+  features {}
 }
 resource "azurerm_resource_group" "tf" {
-  name = "tf-rg"
+  name     = "tf-rg"
   location = "UK South"
 }
 resource "azurerm_resource_group" "tf1" {
-  name = "tf1-rg"
+  name     = "tf1-rg"
   location = azurerm_resource_group.tf.location
 }
